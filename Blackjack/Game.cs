f476523e6510948efb4e4b79c10902905aaad2da
Luke_sinclair <Lukesin@outlook.com>
2025-09-24 12:42:14 +0200

    adding advanced bets and fixed some minor issuse

--- Blackjack/Game.cs
+++ Blackjack/Game.cs
@@ -20,7 +20,7 @@ namespace Blackjack
         bool gameLoop = true;
         // sets up the game and as in the cards 
         public Game() {
-            Newdeck();
+            this.deck = Newdeck();
         }
 
         public void GameLoop()
@@ -28,8 +28,8 @@ namespace Blackjack
 
             while (gameLoop)
             {
-
-                Newdeck();
+                
+                this.deck = Newdeck();
 
             Random rng = new Random();
             int n = this.deck.Count;
@@ -51,20 +51,36 @@ namespace Blackjack
 
                 this.turnLoop = true;
 
+                while (this.player.Wager == 0)
+                {
 
-                Console.WriteLine("What about of Money will you wager of your total of "+this.player.Money);
-                this.player.Wager = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("What about of Money will you wager of your total of "+this.player.Money);
+                    try { 
+                        this.player.Wager = int.Parse(Console.ReadLine());
+                        if (this.player.Wager <= this.player.Money)
+                        { 
+                        this.player.Money -= this.player.Wager;
+                        }else 
+                        {
+                            this.player.Wager = 0;
+                            continue;
+                        }
 
-                this.player.Money -= this.player.Wager;
+                    } catch { }
+                }
 
-            this.dealer.Hand.Add(this.deck[0]);
-            this.deck.Remove(this.dealer.Hand[this.dealer.Hand.Count - 1]);
+                this.dealer.Hand.Add(this.deck[0]);
+                this.deck.Remove(this.dealer.Hand[this.dealer.Hand.Count - 1]);
+                
+                this.player.Hand.Add(this.deck[0]);
+                this.deck.Remove(this.player.Hand[this.player.Hand.Count - 1]);
+                
+                this.dealer.Hand.Add(deck[0]);
+                this.deck.Remove(this.dealer.Hand[this.dealer.Hand.Count - 1]);
 
-
-            this.player.Hand.Add(this.deck[0]);
-            this.deck.Remove(this.player.Hand[this.player.Hand.Count - 1]);
-            this.player.Hand.Add(this.deck[0]);
-            this.deck.Remove(this.player.Hand[this.player.Hand.Count - 1]);
+                this.player.Hand.Add(this.deck[0]);
+                this.deck.Remove(this.player.Hand[this.player.Hand.Count - 1]);
 
             
             while (this.turnLoop)
@@ -171,8 +187,6 @@ namespace Blackjack
 
             if (this.dealer.Dealerturn(scoreCalc(this.dealer.Hand)))
             {
-                this.dealer.Hand.Add(deck[0]);
-                this.deck.Remove(this.dealer.Hand[this.dealer.Hand.Count - 1]);
 
                 if (scoreCalc(this.dealer.Hand) > 21)
                 {
@@ -199,10 +213,29 @@ namespace Blackjack
                 "score:" + score + "\n" +
                 "you have this much: "+this.player.Money+" money beyon what you wagered\n");
 
-            Console.WriteLine("" +
+            Console.WriteLine("dealers shown Card:");
+            Console.WriteLine(this.dealer.Hand[0].Name + " of " + this.dealer.Hand[0].Suit);
+
+            Console.WriteLine("\n" +
                 "Type HIT to get a other card \n" +
-                "Type STAND to be done getting cards \n" +
-                "Anything else and the dealer will eject you from the table \n");
+                "Type STAND to be done getting cards");
+            if ((this.player.Hand.Count == 2) && (this.player.Money>=this.player.Wager))
+            {
+                Console.WriteLine("type Double to double your wager and get only one card");
+
+            }
+            
+            if (this.player.Hand.Count == 2 && this.player.Hand[0].Name.Equals(this.player.Hand[1].Name) && (this.player.Money >= this.player.Wager)) 
+            {
+                Console.WriteLine("type split to split your hand in to two hands");
+            }
+
+            if (this.dealer.Hand[0].Value ==0) 
+            {
+                Console.WriteLine("type insurance to bet that the dealer has natural blackjack");
+            }
+                Console.WriteLine("\n" +
+                    "Anything else and the dealer will eject you from the table \n");
 
             string respone = Console.ReadLine();
 
@@ -215,6 +248,19 @@ namespace Blackjack
                     break;
                 case "STAND":
                     this.turnLoop = false;
+                    break;
+                case "DOUBLE":
+                    this.player.Money -= this.player.Wager;
+                    this.player.Wager = this.player.Wager*2;
+                    this.player.Hand.Add(this.deck[0]);
+                    this.deck.Remove(this.player.Hand[this.player.Hand.Count - 1]);
+                    this.turnLoop = false;
+                    break;
+                case "SPLIT":
+
+                    break;
+                case "INSURANCE":
+
                     break;
                 default:
                     System.Environment.Exit(1);
@@ -269,8 +315,9 @@ namespace Blackjack
             }
         }
 
-        public void Newdeck()
+        public List<Card> Newdeck()
         {
+            List<Card> cards = new List<Card>();
             for (int d = 1; d <= 1; d++)
             {
                 for (int s = 1; s <= 4; s++)
@@ -279,12 +326,12 @@ namespace Blackjack
                     for (int v = 2; v <= 14; v++)
                     {
                         Card card = new Card(v, s, v);
-                        deck.Add(card);
+                        cards.Add(card);
                     }
                 }
 
             }
-
+            return cards;
         }
 
     }
